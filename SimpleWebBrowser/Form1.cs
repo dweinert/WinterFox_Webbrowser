2ef2c0212fbac1114a19a9d10012533cad23f7e2
Daniel Weinert <d.weinert@live.com>
2016-12-01 16:51:08 -0700

    Version 0.1

---
+++ SimpleWebBrowser/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This function is called when the exit menu item is selected
+        //Close the program
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //This function is called when the about menu item is selected
+        //Displays an message
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made by Daniel Weinert");
+        }
+
+        //On click of this button the web control will display the page requested in the text box
+        private void button1_Click(object sender, EventArgs e)
+        {
+            navigateToPage();
+        }
+
+        private void navigateToPage()
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        //This function will fire every single time a key is pushed and released
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                navigateToPage();
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            
+        }
+    }
+}
